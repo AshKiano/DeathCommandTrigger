e8df9416efeef56709c5ecb139ca5c6647971705
jvyskocil <ash.kiano@gmail.com>
2024-03-28 13:18:13 +0100

    version 1.3

--- src/main/java/com/ashkiano/deathcommandtrigger/DeathCommandTrigger.java
+++ src/main/java/com/ashkiano/deathcommandtrigger/DeathCommandTrigger.java
@@ -2,6 +2,12 @@ package com.ashkiano.deathcommandtrigger;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DeathCommandTrigger extends JavaPlugin {
     private FileConfiguration config = getConfig();
@@ -16,9 +22,9 @@ public class DeathCommandTrigger extends JavaPlugin {
         // Register event listener
         getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
 
-        // Print a message to the console to indicate the plugin has been enabled
-        getLogger().info("DeathCommandTrigger has been enabled.");
         getLogger().info("Thank you for using the DeathCommandTrigger plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
+
+        checkForUpdates();
     }
 
     @Override
@@ -29,5 +35,45 @@ public class DeathCommandTrigger extends JavaPlugin {
 
     public FileConfiguration getPluginConfig() {
         return config;
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
+        }
     }
 }
